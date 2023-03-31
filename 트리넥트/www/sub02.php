7d4adbf0f2af3e2480cb55d14adfa64514e216df
imseoyeong <fearless40@naver.com>
2023-03-31 10:53:24 +0900

    Add files via upload

---
+++ 트리넥트/www/sub02.php
@@ -0,0 +1,103 @@
+<?include "./head.php"?>
+
+
+<div class="sub02_section1">
+  <div class="sub02_section1_t">
+    <h4>왜 트리넥트에서 구매해야 할까요?</h4>
+    <p>트리넥트만의 3가지 서비스를 통해, 조경수를 간단하고 안전하게 구매할 수 있습니다.</p>
+  </div>
+
+  <div class="sub02_sec1_box wrap">
+    <div class="flex_box">
+      <div>
+        <div><img src="img/ic_service01.png" alt="간단한 의뢰서"></div>
+        <div>
+          <h4>간단한 의뢰서</h4>
+          <p>간단하게 의뢰서를 만들어<br>농원 파트너스에 제출할 수 있습니다.</p>
+        </div>
+      </div>
+
+      <div>
+        <div><img src="img/ic_service02.png" alt="비교견적 서비스"></div>
+        <div>
+          <h4>비교견적 서비스</h4>
+          <p>검증된 농원 파트너스에게 견적을 받아보고<br>한 눈에 비교할 수 있습니다.</p>
+        </div>
+      </div>
+
+      <div>
+        <div><img src="img/ic_service03.png" alt="에스크로 서비스"></div>
+        <div>
+          <h4>에스크로 서비스</h4>
+          <p>예치한 수목대금을 농원 파트너스에<br>단계별로 분할/지급하여 수목대금을 보호합니다.</p>
+        </div>
+      </div>
+    </div><!-- flex_box 끝 -->
+  </div>
+</div><!-- sub02_section1 끝 -->
+
+<div class="wrap sub02">
+<div class="sub02_section2">
+  <h2>트리넥트를 통한 조경수 구매 과정</h2>
+
+  <div id="timeline">
+      <div class="timeline-item">
+          <div class="timeline-icon">1</div>
+          <div class="timeline-content none_mar">
+              <img src="img/phone_mok01.png" alt="간단한 견적 의뢰서 작성">
+              <h4>간단한 견적 의뢰서 작성</h4>
+              <p>트리넥트의 '견적 의뢰하기'를 통해<br>
+              간단하게 격적 의뢰서를 작성해주세요.</p>
+          </div>
+      </div>
+      <div class="timeline-item">
+          <div class="timeline-icon">2</div>
+          <div class="timeline-content right">
+            <img src="img/phone_mok02.png" alt="한 눈에 견적 비교">
+            <h4>한 눈에 견적 비교</h4>
+            <p>검증된 트리넥트 파트너스로부터 받은<br>
+            수 많은 견적서를 한 눈에 비교해보세요.</p>
+          </div>
+      </div>
+      <div class="timeline-item">
+          <div class="timeline-icon">3</div>
+          <div class="timeline-content">
+            <img src="img/phone_mok03.png" alt="납품 파트너 선택 및 결제">
+            <h4>납품 파트너 선택 및 결제</h4>
+            <p>비교견적을 통해 채택된 농원 파트너와<br>
+            안전한 거래를 위해 트리넥트로 수목대금을 예치합니다.</p>
+          </div>
+      </div>
+      <div class="timeline-item">
+          <div class="timeline-icon">4</div>
+          <div class="timeline-content right">
+            <img src="img/phone_mok04.png" alt="농원의 납품 승인">
+            <h4>농원의 납품 승인</h4>
+            <p>농원의 최종 납품 승인으로 거래 계약이<br>
+            완료됩니다.</p>
+          </div>
+      </div>
+  </div>
+</div><!-- sub02_section2 끝 -->
+
+
+<!-- <div class="sub02_section3">
+  <h2><img src="img/logo02.png" alt="트리넥트 로고"></h2>
+  <p class="bold">많은 기업 고객님들이, 트리넥트를 통해 조경수를 구매하고 있습니다.</p>
+
+  <div class="sub02_sec3_box">
+    <div>A기업</div>
+    <div>B기업</div>
+    <div>C기업</div>
+  </div>
+  <div class="sub02_sec3_box">
+    <div>D기업</div>
+    <div>E기업</div>
+    <div>F기업</div>
+  </div>
+</div> -->
+<!-- sub02_section3 끝 -->
+</div><!-- wrap 끝 -->
+
+
+<?include "./tail.php"?>
---
+++ 트리넥트/www/sub03.php
@@ -0,0 +1,114 @@
+<?include "./head.php"?>
+
+<div class="sub03">
+
+<div class="sub03_section1">
+  <div class="wrap">
+    <h4>트리넥트 농원 파트너스</h4>
+    <p>트리넥트는 정직한 기준을 통해 농원 파트너스를 선별합니다.</p>
+    <p>조경수 납품 경력과 노하우를 갖춘 농원 파트너스가 정직하고 만족스러운 조경수를 납품합니다.</p>
+  </div>
+</div><!-- sub03_section1 끝 -->
+
+<div class="sub03_section2 wrap">
+  <h2>트리넥트 농원 파트너스가 되면 어떤 혜택을 누릴 수 있나요?</h2>
+  <h4>트리넥트 농원 파트너스만의 4가지 제공 혜택</h4>
+
+  <div class="sub03_sec2_box">
+    <div>
+      <h4>온라인 조경수 판매 서비스</h4>
+      <p>전화와 문자 반복은 NO!<br>
+      온라인 입찰 시스템을 통해 편리한 판매 환경 제공</p>
+    </div>
+    <div>
+      <h4>새로운 조경수 유통 판로 제공</h4>
+      <p>이제는 트리넥트를 통해 전국의 모든 조경회사를<br>
+      고객으로 두어 판매 판로를 늘릴 수 있습니다.</p>
+    </div>
+  </div>
+
+  <div class="sub03_sec2_box">
+    <div>
+      <h4>간단한 견적서 제작, 제출</h4>
+      <p>조경수 단가, 품질사항, 사진첨부 3단계를 통해<br>
+      간단하게 견적서를 제작하여 제출이 가능합니다.</p>
+    </div>
+    <div>
+      <h4>안전한 거래환경</h4>
+      <p>에스크로 서비스를 통해 수목 대금을<br>
+      고객으로부터 예치받은 후 발주가 진행됩니다.</p>
+    </div>
+  </div>
+</div><!-- sub03_section2 끝 -->
+
+<!-- <div class="sub03_section3">
+  <div class="wrap">
+  <div class="sub03_sec3_box">
+    <div>
+      <p>신뢰와 품질을 우선으로 경력과 노하우를 갖춘<br>
+      트리넥트와 함께하는 <span>우리는 트리넥트 파트너스 입니다.</span></p>
+    </div>
+    <div><img src="img/two_logo.png" alt="트리넥트 로고"></div>
+  </div>
+  </div>
+</div> -->
+<!-- sub03_section3 끝 -->
+
+<!-- <div class="sub03_section4 wrap">
+  <ul>
+    <li>
+      <div class="farm_bg"><img src="img/farm01.jpg" alt="농원"></div>
+      <div class="farm_img"><img src="img/farm03.jpg" alt="농원"></div>
+      <div class="text">
+        <h4>한국농원</h4>
+        <p>홍길동 농원</p>
+      </div>
+    </li>
+    <li>
+      <div class="farm_bg"><img src="img/farm03.jpg" alt="농원"></div>
+      <div class="farm_img"><img src="img/farm01.jpg" alt="농원"></div>
+      <div class="text">
+        <h4>한국농원</h4>
+        <p>홍길동 농원</p>
+      </div>
+    </li>
+    <li>
+      <div class="farm_bg"><img src="img/farm01.jpg" alt="농원"></div>
+      <div class="farm_img"><img src="img/farm03.jpg" alt="농원"></div>
+      <div class="text">
+        <h4>한국농원</h4>
+        <p>홍길동 농원</p>
+      </div>
+    </li>
+    <li>
+      <div class="farm_bg"><img src="img/farm01.jpg" alt="농원"></div>
+      <div class="farm_img"><img src="img/farm03.jpg" alt="농원"></div>
+      <div class="text">
+        <h4>한국농원</h4>
+        <p>홍길동 농원</p>
+      </div>
+    </li>
+    <li>
+      <div class="farm_bg"><img src="img/farm03.jpg" alt="농원"></div>
+      <div class="farm_img"><img src="img/farm01.jpg" alt="농원"></div>
+      <div class="text">
+        <h4>한국농원</h4>
+        <p>홍길동 농원</p>
+      </div>
+    </li>
+    <li>
+      <div class="farm_bg"><img src="img/farm01.jpg" alt="농원"></div>
+      <div class="farm_img"><img src="img/farm03.jpg" alt="농원"></div>
+      <div class="text">
+        <h4>한국농원</h4>
+        <p>홍길동 농원</p>
+      </div>
+    </li>
+  </ul>
+</div> -->
+<!-- sub03_section4 끝 -->
+
+
+<div><!-- sub03 끝 -->
+
+<?include "./tail.php"?>
